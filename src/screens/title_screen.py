b3cdd28f19c13e93bda0ee9b4ff765f5d8986dbf
Asher Joju <asherjoju@gmail.com>
2025-02-14 22:14:39 +0530

    Updated Code: Title Screen

--- src/screens/title_screen.py
+++ src/screens/title_screen.py
@@ -1,16 +1,23 @@
-from tkinter import Button, Label, Frame
-import tkinter.font as tkFont
+from tkinter import Button, Frame, Label
+
+from navigation_manager import NavigationManager
+
 
 class TitleScreen(Frame):
     
-    def __init__(self, root, on_new_survey):
-        super().__init__(root)
+    def __init__(self, navigator: NavigationManager):
+        super().__init__(navigator.root)
         
-        self.root = root
-        self.on_new_survey = on_new_survey
+        self.navigator = navigator
+        self.root = navigator.root
         
         self.title_label = Label(self, text="Quick Survey", font=("Arial", 32))
         self.new_survey_button = Button(self, text="New", font=("Arial", 18), command=self.on_new_survey)
         
         self.title_label.place(relx=0.5, rely=0.1, anchor="n")
         self.new_survey_button.place(relx=0.5, rely=0.5, anchor="center")
+
+
+    def on_new_survey(self):
+        # self.navigator.push() TODO
+        pass
